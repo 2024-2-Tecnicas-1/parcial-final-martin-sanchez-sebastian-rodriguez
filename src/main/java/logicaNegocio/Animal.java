86a37fd6d3cd386c4e9c93e3e6b7170065b9f5db
santiag015 <santiago.university21.9@gmail.com>
2024-11-18 08:03:30 -0500

    Se crea clase animal

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -1,5 +1,14 @@
 package logicaNegocio;
 
 public abstract class Animal {
-    // TODO: Aquí va tu código
+    protected TipoHabitat habitat;
+
+    public abstract String emitirSonido();
+
+    public abstract String obtenerDieta();
+
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
 }
+
